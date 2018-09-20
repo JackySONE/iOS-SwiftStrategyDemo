81d9f3cfed3542803031b0a3de194008d870ac28
LaiYanYu <jacky.lai@macrowell.com.tw>
2018-09-20 14:18:53 +0800

    feat: 實作Picker作為折扣收費選擇框

--- StrategyDemo/ViewController.swift
+++ StrategyDemo/ViewController.swift
@@ -14,9 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberTextfild: UITextField!
     @IBOutlet weak var listTextView: UITextView!
     @IBOutlet weak var resultLabel: UILabel!
-    
+    @IBOutlet weak var optionPickerView: UIPickerView!
+
     var total: Float = 0.0
-    
+
+    let options = ["正常收費", "打八折", "打七折","打五折"]
+    var selectedIndex = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,12 +36,28 @@ class ViewController: UIViewController {
         guard let priceString = priceTextField.text, let numberString = numberTextfild.text else {
             return
         }
-        
-        let totalPrices = Float(priceString)! * Float(numberString)!
-        
+
+        var totalPrices: Float = 0.0
+        switch selectedIndex {
+        case 0:
+            totalPrices = Float(priceString)! * Float(numberString)!
+            break
+        case 1:
+            totalPrices = Float(priceString)! * Float(numberString)! * 0.8
+            break
+        case 2:
+            totalPrices = Float(priceString)! * Float(numberString)! * 0.7
+            break
+        case 3:
+            totalPrices = Float(priceString)! * Float(numberString)! * 0.5
+            break
+        default:
+            break
+        }
+
         total = total + totalPrices
         
-        listTextView.text = listTextView.text + "\n單價：\(priceString) 數量：\(numberString) 合計：\(totalPrices)"
+        listTextView.text = listTextView.text + "\n單價：\(priceString) 數量：\(numberString) \(options[selectedIndex]) 合計：\(totalPrices)"
         
         resultLabel.text = "\(total)"
         
@@ -51,5 +71,25 @@ class ViewController: UIViewController {
         listTextView.text = nil
         resultLabel.text = "0.00"
     }
+    
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedIndex = row
+    }
+
+}
