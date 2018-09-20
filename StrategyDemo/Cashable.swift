42e886b29ee82636f1bf8a1358c3095c3a02a67a
LaiYanYu <jacky.lai@macrowell.com.tw>
2018-09-20 15:56:04 +0800

    feat: 現金收費協定（抽象類別）
    定義現金收取的抽象方法，收取現金，參數為原價，返回原價

---
+++ StrategyDemo/Cashable.swift
@@ -0,0 +1,15 @@
+//
+//  Cashable.swift
+//  StrategyDemo
+//
+//  Created by 賴彥宇 on 2018/9/20.
+//  Copyright © 2018年 JackySONE. All rights reserved.
+//
+
+import Foundation
+
+protocol Cashable {
+
+    func acceptCash(money: Float) -> Float
+
+}
